94b9c403b5e1fab71874be43e3ef7b8839b2592b
Philippe Mouchel <philippe.mouchel@savoirfairelinux.com>
2015-05-04 11:27:06 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Semi Required Field
+==================
+
+To be continued..
---
+++ srf.api.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by the "Semi-required Field" module.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * List function to alter field widgets.
+ *
+ * @return array An associative array whose keys are field types and
+*    whose values are an associative array containing:
+ *   - "callback" : callback function name to alter field widgets
+ */
+function hook_srf_alter_widget() {
+  $alter_widget = array();
+
+  $alter_widget['text_long'] = array(
+    'callback' => 'srf_text_long_alter_widget',
+  );
+
+  return $alter_widget;
+}
+
+/**
+ * Alter existant widget alter function name.
+ */
+function hook_srf_alter_widget_alter(&$alter_widget) {
+  // You can modify the widget alter function for one or more field types.
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
+
